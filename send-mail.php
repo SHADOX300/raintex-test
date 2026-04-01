2a0ec908b7188479de631715adead6683943cf0f
SHADOX <alex_m137@abv.bg>
2026-04-01 03:34:06 +0300

    superhosting1

--- send-mail.php
+++ send-mail.php
@@ -1,17 +1,22 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name    = htmlspecialchars($_POST['name']);
-    $email   = htmlspecialchars($_POST['email']);
-    $phone   = htmlspecialchars($_POST['phone']);
-    $message = htmlspecialchars($_POST['message']);
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $message = $_POST["message"];
 
-    $to      = "raintex@abv.bg"; // Your email
+    $to = "raintex@abv.bg"; // your email
+
+    // Encode the subject in UTF-8
     $subject = "Ново съобщение от сайта Raintex";
+    $subject = "=?UTF-8?B?" . base64_encode($subject) . "?=";
+
+    $body = "Име: $name\nИмейл: $email\nТелефон: $phone\nСъобщение:\n$message";
+
     $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-
-    $body = "Име: $name\nИмейл: $email\nТелефон: $phone\n\nСъобщение:\n$message";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/plain; charset=UTF-8\r\n";
 
     if(mail($to, $subject, $body, $headers)){
         echo "<script>alert('Съобщението е изпратено успешно!'); window.location.href='contacts.html';</script>";
